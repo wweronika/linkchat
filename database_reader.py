1c20c7d6ac0b0ce17272243c72ac453d73a4927f
wweronika <wiesioleweronika@gmail.com>
2018-05-06 16:55:23 +0200

    Simple database reader

---
+++ database_reader.py
@@ -0,0 +1,17 @@
+import sys
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+command = ''
+
+while True:
+    command = str(input())
+
+    if command == 'exit':
+        exit(0)
+
+    result = cursor.execute(command)
+    for row in result:
+        print(row)
