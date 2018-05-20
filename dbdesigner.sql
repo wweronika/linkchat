a1c3aee8c01d6a62a0c905cca3300cdedfbb2446
Wint3rmute <mateusz.baczek1998@gmail.com>
2018-05-20 17:51:00 +0200

    vscode fajne środowisko

--- dbdesigner.sql
+++ dbdesigner.sql
@@ -3,7 +3,7 @@ CREATE TABLE Users (
 	login string,
 	password string,
 	salt string,
-	email string
+	email string 
 );
 
 CREATE TABLE Messages (
@@ -28,3 +28,19 @@ CREATE TABLE group_user (
 	groupID integer,
 	userID integer
 );
+
+CREATE TABLE Tokens (
+	User_ID integer,
+	token string,
+	token_data string
+);
+
+
+/*
+
+	Import some users
+
+*/
+
+INSERT INTO Users (login, password, salt, email) values("wint3rmute","111","111","mateusz.baczek1998@gmail.com");
+INSERT INTO Users (login, password, salt, email) values("wweronika","111","111","weronika@aaa.com");
