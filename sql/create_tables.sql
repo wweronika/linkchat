ef3cd13aa6f41bcb1be3da3860e097b99f835adb
wint3rmute <mateusz.baczek1998@gmail.com>
2018-08-27 16:29:08 +0200

    our beloved sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,58 @@
+CREATE TABLE `groups` (
+	`group_id` int NOT NULL AUTO_INCREMENT,
+	`title` TEXT NOT NULL,
+	`last_message` DATETIME NOT NULL,
+	PRIMARY KEY (`group_id`)
+);
+
+CREATE TABLE `user_group` (
+	`group_id` int NOT NULL,
+	`user_id` int NOT NULL
+);
+
+CREATE TABLE `users` (
+	`user_id` int NOT NULL AUTO_INCREMENT,
+	`login` TEXT NOT NULL,
+	`salted_password` TEXT NOT NULL,
+	`salt` TEXT NOT NULL,
+	`email` TEXT NOT NULL,
+	`is_active` bool NOT NULL DEFAULT '0',
+	PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE `Links` (
+	`link_id` int NOT NULL AUTO_INCREMENT,
+	`message_id` int NOT NULL,
+	`date` DATETIME NOT NULL,
+	PRIMARY KEY (`link_id`)
+);
+
+CREATE TABLE `messages` (
+	`message_jd` int NOT NULL AUTO_INCREMENT,
+	`sender_jd` int NOT NULL,
+	`group_id` int NOT NULL,
+	`text` TEXT NOT NULL,
+	`date` DATETIME NOT NULL,
+	`is_link` bool NOT NULL DEFAULT '0',
+	`parent_link_id` int,
+	PRIMARY KEY (`message_jd`)
+);
+
+CREATE TABLE `tokens` (
+	`user_id` int NOT NULL,
+	`token` TEXT NOT NULL
+);
+
+ALTER TABLE `user_group` ADD CONSTRAINT `user_group_fk0` FOREIGN KEY (`group_id`) REFERENCES `groups`(`group_id`);
+
+ALTER TABLE `user_group` ADD CONSTRAINT `user_group_fk1` FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`);
+
+ALTER TABLE `Links` ADD CONSTRAINT `Links_fk0` FOREIGN KEY (`message_id`) REFERENCES `messages`(`message_jd`);
+
+ALTER TABLE `messages` ADD CONSTRAINT `messages_fk0` FOREIGN KEY (`sender_jd`) REFERENCES `users`(`user_id`);
+
+ALTER TABLE `messages` ADD CONSTRAINT `messages_fk1` FOREIGN KEY (`group_id`) REFERENCES `groups`(`group_id`);
+
+ALTER TABLE `messages` ADD CONSTRAINT `messages_fk2` FOREIGN KEY (`parent_link_id`) REFERENCES `Links`(`link_id`);
+
+ALTER TABLE `tokens` ADD CONSTRAINT `tokens_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`);
---
+++ sql/recreate.sql
@@ -0,0 +1,5 @@
+DROP DATABASE linkchatdb
+CREATE DATABASE linkchatdb
+USE linkchatdb
+SOURCE ./create_tables.sql
+SOURCE ./insert_basic_data.sql
